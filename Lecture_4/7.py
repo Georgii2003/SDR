599bc32e2245a5bc81ff2191439dd5acccb50c1e
Bloody_Snow <Georgii200321@gmail.com>
2023-10-12 20:07:01 +0700

    upd 7.py

--- Lecture_4/7.py
+++ Lecture_4/7.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.fftpack import fft, ifft,  fftshift, ifftshift
 
 # Create radio
-sdr = adi.Pluto("ip:192.168.2.1")
+sdr = adi.Pluto("ip:192.168.3.1")
 
 # Configure properties
 sdr.rx_lo = 2452000000
@@ -16,20 +16,13 @@ sdr.rx_lo = 2452000000
 # Collect data
 for r in range(60):
     rx = sdr.rx()
+    N=512
+    X = fft(rx,N)/N
+    plt.figure("FFT")
     plt.clf()
-    plt.plot(rx.real)
-    plt.plot(rx.imag)
+    plt.stem(abs(X))
     plt.ylim([-2000, 2000])
     plt.draw()
-    if rx.imag[10] > 200:
-    	time.sleep(5)
     plt.pause(0.05)
     time.sleep(0.1)
-
-# Были добавлены
-N=512
-X = fft(rx,N)/N
-plt.figure("FFT")
-plt.stem(abs(X))
-    
 plt.show()
